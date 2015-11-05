58fc262057592011e08e705d8a3926c8bebf6136
Matusinka Roland <matusinka.roland@outlook.com>
2015-11-05 13:32:31 +0100

    Lint corrections.

--- SPOC Common/src/main/java/hu/mrolcsi/android/spoc/common/service/DatabaseBuilderService.java
+++ SPOC Common/src/main/java/hu/mrolcsi/android/spoc/common/service/DatabaseBuilderService.java
@@ -52,7 +52,7 @@ import java.util.concurrent.TimeUnit;
 
 public class DatabaseBuilderService extends IntentService {
 
-    public static final String TAG = "SPOC.Common.DatabaseBuilder";
+    public static final String TAG = "SPOC.DatabaseBuilder";
     public static final String BROADCAST_ACTION_IMAGES_READY = "hu.mrolcsi.android.spoc.BROADCAST_IMAGES_READY";
     public static final String BROADCAST_ACTION_FINISHED = "hu.mrolcsi.android.spoc.BROADCAST_DATABASE_READY";
     public static final String ARG_FIRST_START = "SPOC.Common.FIRST_START";
